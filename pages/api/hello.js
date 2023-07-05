f38021a573b916b92631185bc0e343e13b4fe485
Manchester-United-in-my-heart <129467586+BuiDuongKham@users.noreply.github.com>
2023-07-05 09:10:45 +0700

    puzzle GUI

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log("I am being called")
+  console.log(req.body)
+  res.status(200).json(req.body)
 }
